f62342f8ce0d1ab21cb0c47831fc5e749bede552
Jose Carlos Pereira <jcps.filho@gmail.com>
2022-06-03 19:07:59 -0300

    Aplicando etapa de rafatoracao de codigo

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -8,22 +8,7 @@ import br.com.alura.tdd.modelo.Funcionario;
 public class ReajusteService {
 
 	public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-		BigDecimal reajuste = null;
-		
-		switch (desempenho) {
-			case A_DESEJAR:
-				reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-				break;
-			case BOM:
-				reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-				break;
-			case OTIMO:
-				reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-				break;
-			default:
-				break;
-		}
-		
+		BigDecimal reajuste = funcionario.getSalario().multiply(desempenho.percentualReajuste());
 		funcionario.reajustarSalario(reajuste);
 	}
 
