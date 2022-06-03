a6330c90b53495114c466f8a4c88766a37b976e4
Jose Carlos Pereira <jcps.filho@gmail.com>
2022-06-03 19:54:37 -0300

    Realizando refatoração dos testes da class ReajusteService

--- src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.tdd.modelo.Desempenho;
@@ -12,33 +13,30 @@ import br.com.alura.tdd.modelo.Funcionario;
 
 public class ReajusteServiceTest {
 	
+	private ReajusteService service;
+	private Funcionario funcionario;
+
+	@BeforeEach
+	public void init() {
+		service = new ReajusteService();
+		funcionario = new Funcionario("Teste", LocalDate.now(), new BigDecimal("1000.00"));
+	}
+	
 	@Test
 	public void reajusteDeveSerTresPorcentoQuandoDesempenhoForADesejar() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Teste", LocalDate.now(), new BigDecimal("1000.00"));
-		
 		service.concederReajuste(funcionario, Desempenho.A_DESEJAR);
-		
 		assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
 	}
 	
 	@Test
 	public void reajusteDeveSerQuinzePorcentoQuandoDesempenhoForADesejar() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Teste", LocalDate.now(), new BigDecimal("1000.00"));
-		
 		service.concederReajuste(funcionario, Desempenho.BOM);
-		
 		assertEquals(new BigDecimal("1150.00"), funcionario.getSalario());
 	}
 	
 	@Test
 	public void reajusteDeveSerVintePorcentoQuandoDesempenhoForADesejar() {
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("Teste", LocalDate.now(), new BigDecimal("1000.00"));
-		
 		service.concederReajuste(funcionario, Desempenho.OTIMO);
-		
 		assertEquals(new BigDecimal("1200.00"), funcionario.getSalario());
 	}
 
